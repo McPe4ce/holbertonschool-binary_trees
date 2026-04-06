ac4212e4553de70893aa09881b3ef0753313e6ae
Mcpe4ce <phil.ghanem@gmail.com>
2026-04-06 14:41:17 +0200

    Fix formatting and indentation in binary_tree_is_leaf function

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -3,17 +3,18 @@
 /**
  * binary_tree_is_leaf - Checks if a node is a leaf
  * @node: Node to check if it is a leaf
+ * Return: 1 (if it is a leaf)
  */
 int binary_tree_is_leaf(const binary_tree_t *node)
 {
-    if (node == NULL)
-    {
-        return (0);
-    }
-    
-    if (node->left == NULL && node->right == NULL)
-    {
-        return (1);
-    }
-    return (0);
+	if (node == NULL)
+	{
+		return (0);
+	}
+	
+	if (node->left == NULL && node->right == NULL)
+	{
+		return (1);
+	}
+	return (0);
 }
